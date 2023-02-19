e88fef4b2ca0c81986742b9e60e5082a4088f144
Mihran <mihranprof@gmail.com>
2023-02-19 17:11:54 -0500

    first command

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react'
 export default  class Resume extends Component {
   render() {
     let resumeData = this.props.resumeData;
